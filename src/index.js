69993e123c9b259eaa9cec4f144394e033b9280f
Nidhi Singh <nidhi4upooja@gmail.com>
2025-09-01 13:02:34 +0530

    connected to mongodb server

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ const express = require("express");
 
 const ServerConfig = require("./config/serverConfig");
 
+const connectDB = require("./config/dbConfig");
+
 const app = express();
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
+  await connectDB();
   console.log(`server is running ${ServerConfig.PORT}`);
 });
+
+// password = JzV8tXU41CtrNnUR
+//username =  nidhiupooja7_db_user
+
+//mongodb+srv://nidhiupooja7_db_user:JzV8tXU41CtrNnUR@cluster0.ieuscqp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
