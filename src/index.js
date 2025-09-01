d49ee4e3c94750c063f1a0e5c7cfda69b6a61203
Nidhi Singh <nidhi4upooja@gmail.com>
2025-09-01 14:03:44 +0530

    Added body parser

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const ServerConfig = require("./config/serverConfig");
 
 const connectDB = require("./config/dbConfig");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded());
 
 app.listen(ServerConfig.PORT, async () => {
   await connectDB();
