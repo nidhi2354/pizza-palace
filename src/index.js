edf383c3d5a11e7802b854c8023d1ee6947597f1
Nidhi Singh <nidhi4upooja@gmail.com>
2025-09-18 12:00:14 +0530

    added isLoggedIn

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const ServerConfig = require("./config/serverConfig");
 const connectDB = require("./config/dbConfig");
 const userRouter = require("./routes/userRoute");
 const authRoute = require("./routes/authRoute");
+const { isLoggedIn } = require("./validations/authValidator");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 //Routing middleware
 //if your req route starts with /users then handle it using userRouter
 app.use("/users", userRouter); // Connects the router to the server
 app.use("/auth", authRoute);
+
+app.get("/ping", isLoggedIn, (req, res) => {
+  console.log(req.body);
+  console.log(req.cookies);
+  return res.json({ message: "pong" });
+});
 
 app.listen(ServerConfig.PORT, async () => {
   await connectDB();
